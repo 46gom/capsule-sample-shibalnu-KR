030505f125791920a2d228d08b78ca42738b7e24
j-peter-jeon <j_peter.jeon@samsung.com>
2019-04-05 14:14:58 +0900

    Fix: Refactoring Capsule

--- code/showImg.js
+++ code/showImg.js
@@ -1,20 +1,24 @@
-// 설정한 property(capsule.properties)로부터 데이터를 가져옵니다.
-const baseURL = config.get("baseUrl");
-
 module.exports.function = function showImg (query) {
+  const config = require('config');
+  const fail = require('fail');
+  const http = require('http');
+  const console = require('console');
+  // 설정한 property(capsule.properties)로부터 데이터를 가져옵니다.
+  const baseURL = config.get("baseUrl");
+  
   if(query.length === 0){
     throw fail.checkedError("No Result", "NoResult");  
   }
   
-  var mode = query.mode.random;
-  var response = null;
-  var result = {};
+  let mode = query.mode.random;
+  let response = null;
+  let result = {};
   
   if(query.mode.images == true){
     url = baseURL + '15';
     
-    //외부 API로 부터 데이터를 받습니다. (https://bixbydevelopers.com/dev/docs/reference/JavaScriptAPI/http)
-    // cacheTime: cache 시간을 설정합니다, returnHeaders: API에 대한 Response를 Header 형식으로 받습니다.
+    // 외부 API로 부터 데이터 받음 (https://bixbydevelopers.com/dev/docs/reference/JavaScriptAPI/http)
+    // cacheTime: cache 시간을 설정, returnHeaders: API에 대한 Response를 Header 형식으로 받음.
     response = http.getUrl(url, {format:"json", cacheTime: 0, returnHeaders:true});
     
     if(response.status == 404){
